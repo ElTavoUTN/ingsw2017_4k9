3f85f615bff70c01df43b2be76bca9e30c07702a
Matias <matias_corvalan@outlook.com>
2017-08-24 10:23:40 -0300

    Se puede ejecutar desde el main

--- TP5IngSw4k9/src/tp5ingsw4k9/TP5IngSw4k9.java
+++ TP5IngSw4k9/src/tp5ingsw4k9/TP5IngSw4k9.java
@@ -1,9 +1,14 @@
 package tp5ingsw4k9;
 
+import Vista.Principal;
+
 public class TP5IngSw4k9 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        if (args.length == 0){
+            Principal actual = new Principal();
+            actual.setVisible(true);
+            System.out.println("Pantalla principal");
+        }
     }
-    
 }
